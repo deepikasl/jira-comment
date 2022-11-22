6d17775fd9a794e647ace6cc6b06fbab197d9aae
Deepika <18304961+deepikasl@users.noreply.github.com>
2022-11-22 14:37:57 +0530

    Update jira-comment.js

--- src/jira-comment.js
+++ src/jira-comment.js
@@ -1,7 +1,32 @@
-const setupGo = require("./setupGo");
+const tasks = require('jfrog-pipelines-tasks');
+const axios = require('axios');
 
-function main() {
-  setupGo.run();
+async function jira_comment() {
+  const issue_key = tasks.getInput('issue_key');
+  let endpoint = tasks.getInput('endpoint');
+  const username = tasks.getInput('username');
+  const token = tasks.getInput('token');
+  const comment = tasks.getInput('comment');
+  const apiSuffix = "/rest/api/2";
+
+  endpoint = endpoint.replace(/^\/|\/$/g, ''); // removes trailing slash
+  endpoint = endpoint.replace(apiSuffix, '');
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    auth: {
+      username: username,
+      password: token
+    },
+    data: {
+      'body': comment
+    },
+    url: endpoint + '/rest/api/2/issue/' + issue_key + '/comment'
+  };
+  await axios(options);
 }
 
-main();
+module.exports = jira_comment;
