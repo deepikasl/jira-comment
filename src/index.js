f8e542806d6fda176e5770b2d87d3d0bbcf45ac9
Deepika <18304961+deepikasl@users.noreply.github.com>
2022-11-22 14:38:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
-const tasks = require('jfrog-pipelines-tasks');
-const axios = require('axios');
+const jira_comment = require("./jira_comment");
 
-async function main() {
-  const issue_key = tasks.getInput('issue_key');
-  let endpoint = tasks.getInput('endpoint');
-  const username = tasks.getInput('username');
-  const token = tasks.getInput('token');
-  const comment = tasks.getInput('comment');
-  const apiSuffix = "/rest/api/2";
-
-  endpoint = endpoint.replace(/^\/|\/$/g, ''); // removes trailing slash
-  endpoint = endpoint.replace(apiSuffix, '');
-
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    auth: {
-      username: username,
-      password: token
-    },
-    data: {
-      'body': comment
-    },
-    url: endpoint + '/rest/api/2/issue/' + issue_key + '/comment'
-  };
-  await axios(options);
+function main() {
+  jira_comment.run();
 }
-  
-(
-  async () => {
-    await main();
-  }
-)();
+
+main();
